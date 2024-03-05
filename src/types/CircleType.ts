5176a10da44946cbfda2b147f38c6938969c912c
Eleonora Di Pierro <eleonora.dipierro@gmail.com>
2024-03-05 11:39:01 +0100

    [svg][type] add palette svg with its type

---
+++ src/types/CircleType.ts
@@ -0,0 +1,10 @@
+export type CircleProps = {
+  height: string;
+  width: string;
+  fillPrimary?: string;
+  fillSecondary?: string;
+  fill?: string;
+  opacity: string;
+  stroke: string;
+  strokeWidth?: string | number;
+};
