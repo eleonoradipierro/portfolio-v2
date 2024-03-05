0b3edb6b4b340500f5dc6cd62bca91f81d339986
Eleonora Di Pierro <eleonora.dipierro@gmail.com>
2024-03-05 17:24:21 +0100

    [style][feat] add contacts component

---
+++ src/components/Contacts/Contacts.tsx
@@ -0,0 +1,38 @@
+import { Children } from "react";
+import { PaletteProps } from "../../types/SettingsType";
+import GitHubIcon from "../SvgComponents/GitHubIcon";
+import LinkedinIcon from "../SvgComponents/LinkedinIcon";
+import MailIcon from "../SvgComponents/MailIcon";
+import PhoneIcon from "../SvgComponents/PhoneIcon";
+import styles from "./Contacts.module.scss";
+
+const Contacts = (props: PaletteProps) => {
+  const { backgroundColor } = props;
+
+  return (
+    <ul className={styles.container}>
+      <li>
+        <a href="mailto:eleonora.dipierro@gmail.com">
+          <MailIcon height="20px" width="20px" fill={backgroundColor} />
+        </a>
+      </li>
+      <li>
+        <a href="tel:+393343451004">
+          <PhoneIcon height="20px" width="20px" fill={backgroundColor} />
+        </a>
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/in/eleonoradipierro/" target="_blank">
+          <LinkedinIcon height="20px" width="20px" fill={backgroundColor} />
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/eleonoradipierro" target="_blank">
+          <GitHubIcon height="20px" width="20px" fill={backgroundColor} />
+        </a>
+      </li>
+    </ul>
+  );
+};
+
+export default Contacts;
