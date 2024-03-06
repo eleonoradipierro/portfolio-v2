1160c99acc62ffa135be9c6e69a1ca16f4cf8a09
Eleonora Di Pierro <eleonora.dipierro@gmail.com>
2024-03-06 17:45:58 +0100

    [feat] add dot scrollbar to nav bar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,28 +1,42 @@
 import { NAVBAR_OPTION_LIST } from "../../costants";
 import styles from "./NavBar.module.scss";
 import { NavBarProps } from "../../types/NavBarType";
+import DotScrollbar from "../DotScrollbar/DotScrollbar";
 
 const NavBar = (props: NavBarProps) => {
   const { shownSection, setShownSection } = props;
-
+  const textColor = "#3F3114";
+  const primaryColor = "#D4C5A6";
+  const secondaryColor = "#B1A07D";
   return (
-    <nav className={styles.options_container}>
-      {NAVBAR_OPTION_LIST.map((option) => {
-        return (
-          <button
-            key={option}
-            onClick={() => {
-              setShownSection(option);
-            }}
-            style={
-              shownSection === option ? { textDecoration: "underline" } : {}
-            }
-          >
-            {option}
-          </button>
-        );
-      })}
-    </nav>
+    <div>
+      <nav className={styles.options_container}>
+        {NAVBAR_OPTION_LIST.map((option) => {
+          return (
+            <button
+              key={option}
+              onClick={() => {
+                setShownSection(option);
+              }}
+              style={
+                shownSection === option ? { textDecoration: "underline" } : {}
+              }
+            >
+              {option}
+            </button>
+          );
+        })}
+      </nav>
+      <DotScrollbar
+        {...{
+          shownSection,
+          setShownSection,
+          textColor,
+          primaryColor,
+          secondaryColor,
+        }}
+      />
+    </div>
   );
 };
 
