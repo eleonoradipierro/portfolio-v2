4a3e4cf79c50f2956f9b389dccd53d63d9b290fd
Eleonora Di Pierro <eleonora.dipierro@gmail.com>
2024-03-06 18:02:47 +0100

    [feat] add child element

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,10 +4,7 @@ import { NavBarProps } from "../../types/NavBarType";
 import DotScrollbar from "../DotScrollbar/DotScrollbar";
 
 const NavBar = (props: NavBarProps) => {
-  const { shownSection, setShownSection } = props;
-  const textColor = "#3F3114";
-  const primaryColor = "#D4C5A6";
-  const secondaryColor = "#B1A07D";
+  const { shownSection, setShownSection, children } = props;
   return (
     <div>
       <nav className={styles.options_container}>
@@ -27,6 +24,7 @@ const NavBar = (props: NavBarProps) => {
           );
         })}
       </nav>
+      {children}
       <DotScrollbar
         {...{
           shownSection,
--- src/types/NavBarType.ts
+++ src/types/NavBarType.ts
@@ -1,10 +1,9 @@
-import { NAVBAR_OPTIONS } from "../enums";
+import { ReactNode } from "react";
 import { NavBarOption } from "../global/enums";
 import { State } from "./GlobalTypes";
 
 export type NavBarProps = {
   shownSection: NavBarOption;
   setShownSection: State<NavBarOption>;
-  shownSection: NAVBAR_OPTIONS;
-  setShownSection: State<NAVBAR_OPTIONS>;
+  children: ReactNode;
 };
