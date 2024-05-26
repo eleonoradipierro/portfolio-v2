3561accc8ba5c1c25a5ae5a14260b0a66129b091
Eleonora Di Pierro <eleonora.dipierro@gmail.com>
2024-05-26 12:33:56 +0200

    [refactor] updated theme context

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,65 +1,35 @@
 import { useState, createContext } from "react";
 import { PRESET_PALETTE } from "../global/costants";
 import { ThemeProps } from "../types/SettingsType";
-import { LabelType } from "../types/GlobalTypes";
 import {
   PaletteContextType,
   PaletteNewThemeContextType,
   ThemeProviderProps,
 } from "../types/ThemeContextType";
 
+const getRandomInt = (max: number) => {
+  return Math.floor(Math.random() * max);
+};
+
 export const PaletteContext = createContext(PRESET_PALETTE[0]);
 export const PaletteUpdateContext = createContext<PaletteContextType>(() => {});
 export const PaletteNewThemeContext = createContext<PaletteNewThemeContextType>(
-  () => {}
+  () => {},
 );
 
 const ThemeProvider = (props: ThemeProviderProps) => {
   const { children } = props;
 
-  const [theme, setTheme] = useState(PRESET_PALETTE[0]);
+  const [theme, setTheme] = useState(PRESET_PALETTE[getRandomInt(7)]);
 
   const toggleTheme = (newTheme: ThemeProps) => {
     setTheme(newTheme);
   };
 
-  const setNewTheme = (label: LabelType, color: string) => {
-    const newTheme: ThemeProps = {
-      backgroundColor: theme.backgroundColor,
-      primaryColor: theme.primaryColor,
-      secondaryColor: theme.secondaryColor,
-      textColor: theme.secondaryColor,
-      title: "",
-    };
-
-    // TODO
-    // crea un oggetto del tipo {label : "background", index: "backgroundColor", value:"#pippo"}
-    // per poter fare questa cosa meglio -> newTheme[label] = color
-
-    switch (label) {
-      case "background":
-        newTheme.backgroundColor = color;
-        break;
-      case "primary":
-        newTheme.primaryColor = color;
-        break;
-      case "secondary":
-        newTheme.secondaryColor = color;
-        break;
-      case "text":
-        newTheme.textColor = color;
-        break;
-    }
-    setTheme(newTheme);
-    console.log(label, theme);
-  };
-
   return (
     <PaletteContext.Provider value={theme}>
       <PaletteUpdateContext.Provider value={toggleTheme}>
-        <PaletteNewThemeContext.Provider value={setNewTheme}>
-          {children}
-        </PaletteNewThemeContext.Provider>
+        {children}
       </PaletteUpdateContext.Provider>
     </PaletteContext.Provider>
   );
